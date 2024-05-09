9717bd3be1406753e2bcf918ed7605937e55238f
bugsounet <github@bugsounet.fr>
2024-05-09 14:21:05 +0200

    missing canStop

--- EXT-RadioPlayer.js
+++ EXT-RadioPlayer.js
@@ -186,6 +186,7 @@ Module.register("EXT-RadioPlayer", {
     if (this.radioPlayer.play) {
       this.sendNotification("EXT_RADIO-CONNECTED");
       this.show(1000, () => {}, { lockString: "EXT-RADIO_LOCK" });
+      this.canStop = true;
     } else {
       this.sendNotification("EXT_RADIO-DISCONNECTED");
       this.hide(1000, () => {}, { lockString: "EXT-RADIO_LOCK" });
